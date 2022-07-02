737dd1431fab8ab9719105f7d2023232c0fa9386
Rumeysa Tan <tanrumeysa93@gmail.com>
2022-07-02 13:40:58 +0300

    Initial commit

---
+++ rChat/rChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rChat
+//
+//  Created by Rumeysa TAN on 2.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
