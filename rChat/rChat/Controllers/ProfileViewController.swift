8cb853eae1e33d6b6effb9ef6615c8aa69f36c97
Rumeysa Tan <tanrumeysa93@gmail.com>
2022-07-04 22:39:47 +0300

    log out added

--- rChat/rChat/Controllers/ProfileViewController.swift
+++ rChat/rChat/Controllers/ProfileViewController.swift
@@ -6,12 +6,53 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ProfileViewController: UIViewController {
+    
+    @IBOutlet var tableView : UITableView!
+    
+    let data = ["Log Out"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "cell")
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
+}
+
+extension ProfileViewController : UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        cell.textLabel?.textAlignment = .left
+        cell.textLabel?.textColor = .systemIndigo
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        do {
+            try FirebaseAuth.Auth.auth().signOut()
+            
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: true)
+        }
+        catch{
+            print("Faild to log out")
+        }
+        
+    }
 }
